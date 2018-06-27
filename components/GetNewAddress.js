cdfb489a7bee459209a754c3df8d2d0b3b3b0842
Skylar Saveland <skylar.saveland@gmail.com>
2018-06-26 21:18:48 -0700

    GetNewAddress, let's settle on 2 spaces

---
+++ components/GetNewAddress.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  StyleSheet,
+  View, 
+  Text,
+  TextInput,
+  Button,
+} from 'react-native'
+import { connect } from 'react-redux'
+import stdrpc from 'stdrpc'
+
+import BlockData from 'components/BlockData'
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  field: {
+  },
+  label: {
+  },
+  input: {
+  },
+  button: {
+    width: 120,
+    height: 60,
+  }
+})
+
+class GetNewAddress extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newaddress: false
+    }
+  }
+  create() {
+    const c = stdrpc({
+      url: 'http://localhost:8232',
+      username: this.props.auth.username,
+      password: this.props.auth.password,
+    })
+    c.getnewaddress().then((newaddress) => {
+      this.setState({ newaddress })
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.newaddress && 
+          <Text>{this.state.newaddress}</Text>}
+        }
+        <Button
+            style={styles.button}
+            title="Get New Address"
+            onPress={() => this.create()}
+        />
+      </View>
+    )
+  }
+}
+
+export default connect((state) => {
+  return {
+    auth: state.auth,
+  }
+})(GetNewAddress)
--- components/XferScreen.js
+++ components/XferScreen.js
@@ -6,16 +6,18 @@ import {
 } from 'react-native'
 
 import GetBalance from 'components/GetBalance'
+import GetNewAddress from 'components/GetNewAddress'
 
 
 export default class SendRecieveContainer extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Xfers</Text>
-                <GetBalance />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Text>Xfers</Text>
+        <GetBalance />
+        <GetNewAddress />
+      </View>
+    )
+  }
 }
   
