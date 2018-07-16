6e259bf2030d50fa913f7ad940425d4ec890afcc
Skylar Saveland <skylar.saveland@gmail.com>
2018-07-15 23:45:02 -0700

    misc: play with WalletScreen layout on OSX

--- components/WalletScreen.js
+++ components/WalletScreen.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
 import {
-  // StyleSheet,
+  StyleSheet,
   Text,
-  View,
+  ScrollView,
 } from 'react-native'
 
+import ListTransactions from 'components/stateful/ListTransactions'
 import ZGetOperationStatus from 'components/ZGetOperationStatus'
 import ZShieldCoinbase from 'components/ZShieldCoinbase'
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 2,
+    width: '100%',
+    flex: 1,
+    // alignItems: 'center',
+  },
+})
 
 
 export default class WalletScreen extends Component {
   render() {
     return (
-      <View>
+      <ScrollView contentContainerStyle={styles.container}>
         <Text>Wallet</Text>
+        <ListTransactions />
         <ZGetOperationStatus />
         {/* TODO: how to know if there is anything to shield? */}
         <ZShieldCoinbase />
-      </View>
+      </ScrollView>
     )
   }
 }
